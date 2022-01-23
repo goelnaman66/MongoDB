7e9034c380a49b3af78da9c88e2b6131de17ddc3
Naman Goel <goelnaman66@gmail.com>
2022-01-24 00:58:48 +0530

    Step 2:adding Queries

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 const Mongoose = require('mongoose');
+const User = require('./user')
+const UserQuery = require('./userModelImpl');
 
 const connect = () =>{
-   return Mongoose.connect('mongodb://localhost:27017/UnacademyMongoDemo');
+   return Mongoose.connect('mongodb://localhost:27017/MongoDemo');
 };
 
 
 connect()
-.then(connection => {
-    console.log("Mongo connection Successful");
+.then(async connection => {
+    console.log('Mongo DB Connection Setup Done!!');
+
+    // Just For Demo
+    await User.deleteMany({});
+    // await Blog.deleteMany({});
+    //
+
+    const user = await User.create({
+        firstName: 'Sanket',
+        lastName: 'Singh',
+        email: 'ss@gmail.com'
+    });
+    console.timeLog(user);
+
+    await User.create([
+        {
+            firstName: 'Sarthak',
+            lastName: 'Jain',
+            email: 'sj@gmail.com'
+        }, 
+        {
+            firstName: 'Upkar',
+            email: 'u@gmail.com',
+            underAge: true
+        }
+    ])
+
+    
+    console.log("******")
+    const u1 = await UserQuery.getAllUsers();
+    const u2 = await UserQuery.getUserById(user.id);
+    const u3 = await UserQuery.updateUserByID(user.id, {underAge: true});
+    console.log(u1);
+    console.log(u2);
+    console.log(u3);
+
 })
 .catch(err=>console.log(err));
